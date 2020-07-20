5214306248fcd149d4590a8a68705d2358d1a8e0
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-19 22:38:11 -0400

    Remove URI

--- day3/lyrical-graphql/server/db.js
+++ day3/lyrical-graphql/server/db.js
@@ -2,8 +2,7 @@
 const mongoose = require("mongoose");
 
 // Replace with your Mongodb URI
-const MONGO_URI =
-  "mongodb+srv://dbUser:ITrvnUU4RvboACp6@cluster0.va4e7.mongodb.net/lyricaldb?retryWrites=true&w=majority";
+const MONGO_URI = "";
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoDB URI");
 }
