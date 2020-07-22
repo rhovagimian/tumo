e7f3cc34b1fa143f03b2e11f4e9d3c1ee3497c27
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-22 00:21:08 -0400

    Wire up mutation

--- day4/lyrical-graphql/client/components/SongCreate.js
+++ day4/lyrical-graphql/client/components/SongCreate.js
@@ -1,10 +1,40 @@
-import React from "react";
+//@ts-check
+import React, { useState } from "react";
+import { graphql, commitMutation } from "react-relay";
+import environment from "../relay/environment";
+
+const mutation = graphql`
+  mutation SongCreate_Mutation($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
 
 function SongCreate() {
+  const [title, onTitleChange] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    commitMutation(environment, {
+      mutation,
+      variables: {
+        title,
+      },
+    });
+  };
   return (
     <div>
       <h3>Create a New Song</h3>
-      <form></form>
+      <form onSubmit={onSubmit}>
+        <label>Song Create:</label>
+        <input
+          onChange={(event) => {
+            onTitleChange(event.target.value);
+          }}
+          value={title}
+        />
+      </form>
     </div>
   );
 }
