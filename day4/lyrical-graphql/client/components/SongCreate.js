a69a40c2d6362006e5bd99805b37b9654454b5f1
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-21 22:12:29 -0400

    Add Song Create

---
+++ day4/lyrical-graphql/client/components/SongCreate.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SongCreate() {
+  return (
+    <div>
+      <h3>Create a New Song</h3>
+      <form></form>
+    </div>
+  );
+}
+
+export default SongCreate;
--- day4/lyrical-graphql/client/index.js
+++ day4/lyrical-graphql/client/index.js
@@ -1,10 +1,25 @@
 //@ts-check
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SongCreate from "./components/SongCreate";
 import SongList from "./components/SongList";
 
 const Root = () => {
-  return <SongList />;
+  return (
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route path="/song/create">
+            <SongCreate />
+          </Route>
+          <Route path="/">
+            <SongList />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
