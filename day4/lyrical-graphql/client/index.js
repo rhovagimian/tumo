a2b8571c6e7f35df5e4549e7a2342bba59a1786b
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-22 08:35:29 -0400

    Add styling

--- day4/lyrical-graphql/client/index.js
+++ day4/lyrical-graphql/client/index.js
@@ -1,4 +1,5 @@
 //@ts-check
+import "./style/style.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
--- day4/lyrical-graphql/server/db.js
+++ day4/lyrical-graphql/server/db.js
@@ -2,7 +2,8 @@
 const mongoose = require("mongoose");
 
 // Replace with your Mongodb URI
-const MONGO_URI = "";
+const MONGO_URI =
+  "mongodb+srv://dbUser:0JB2dLYVWyUFekYY@cluster0.va4e7.mongodb.net/lyricaldb?retryWrites=true&w=majority";
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoDB URI");
 }
