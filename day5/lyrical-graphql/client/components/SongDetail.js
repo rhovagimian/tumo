defa662246f65229e03fef6f5ac95489f270796d
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-26 15:21:47 -0400

    Add QueryRenderer

--- day5/lyrical-graphql/client/components/SongDetail.js
+++ day5/lyrical-graphql/client/components/SongDetail.js
@@ -1,11 +1,39 @@
 //@ts-check
 import React from "react";
+import { graphql, QueryRenderer } from "react-relay";
+import { useParams } from "react-router-dom";
+import environment from "../relay/environment";
 
-function SongDetail() {
+const query = graphql`
+  query SongDetail_Query($id: ID!) {
+    song(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+function renderQuery({ error, props }) {
+  if (!error && !props) {
+    return <div>Loading...</div>;
+  }
+  const { song } = props;
   return (
     <div>
-      <h3>Song Detail</h3>
+      <h3>{song.title}</h3>
     </div>
+  );
+}
+
+function SongDetail() {
+  const { id } = useParams();
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={query}
+      variables={{ id }}
+      render={renderQuery}
+    />
   );
 }
 
