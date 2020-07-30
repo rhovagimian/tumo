0a6371231cadbddcca71e9277eb20947a0c5c922
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-29 23:19:16 -0400

    Add header component

--- day7/auth-graphql/client/index.js
+++ day7/auth-graphql/client/index.js
@@ -1,9 +1,18 @@
 //@ts-check
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 const Root = () => {
-  return <div>Auth Starter</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/"></Route>
+      </Switch>
+    </Router>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
