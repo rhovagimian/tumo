95c2244e04dcba38d0192e5c4d2f2fb1f1b18a09
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-31 00:57:12 -0400

    Add Login Form

--- day7/auth-graphql/README.md
+++ day7/auth-graphql/README.md
@@ -3,5 +3,7 @@
 Starter project for an application with authorization and GraphQL
 
 ```
+npm run build-schema
+npm run relay
 npm run dev
 ```
--- day7/auth-graphql/client/index.js
+++ day7/auth-graphql/client/index.js
@@ -3,15 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import LoginForm from "./components/LoginForm";
 
 const Root = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/"></Route>
-      </Switch>
-    </Router>
+    <div className="container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
