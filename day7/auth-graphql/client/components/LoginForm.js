7aa9cbeae27d0085edf79d6f3ae3b2c7d42eb7b0
Raffi Hovagimian <raffi.hovagimian@gmail.com>
2020-07-31 03:06:14 -0400

    Fix Login issue

--- day7/auth-graphql/client/components/LoginForm.js
+++ day7/auth-graphql/client/components/LoginForm.js
@@ -1,5 +1,5 @@
 //@ts-check
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import AuthForm from "./AuthForm";
 import { graphql, commitMutation } from "react-relay";
 import environment from "../relay/environment";
@@ -14,26 +14,29 @@ const mutation = graphql`
 
 function LoginForm() {
   const [errors, setErrors] = useState([]);
+  const onSubmit = useCallback(
+    ({ email, password }) => {
+      commitMutation(environment, {
+        mutation,
+        variables: {
+          email,
+          password,
+        },
+        onCompleted: (response, errors) => {
+          if (errors) {
+            setErrors(errors.map((e) => e.message));
+          } else {
+            setErrors([]);
+          }
+        },
+      });
+    },
+    [errors]
+  );
   return (
     <div>
       <h3>Login</h3>
-      <AuthForm
-        onSubmit={({ email, password }) => {
-          commitMutation(environment, {
-            mutation,
-            variables: {
-              email,
-              password,
-            },
-            onCompleted: (response, errors) => {
-              if (errors) {
-                setErrors(errors.map((e) => e.message));
-              }
-            },
-          });
-        }}
-        errors={errors}
-      />
+      <AuthForm onSubmit={onSubmit} errors={errors} />
     </div>
   );
 }
